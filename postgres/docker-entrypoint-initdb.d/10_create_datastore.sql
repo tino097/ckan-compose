0a43428687fde77c288b52e66caf2609a59c0a1f
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-08-20 15:02:20 +0200

    Updates in vriables

--- postgres/docker-entrypoint-initdb.d/10_create_datastore.sql
+++ postgres/docker-entrypoint-initdb.d/10_create_datastore.sql
@@ -1,4 +1,4 @@
 \set datastore_ro_password '\'' `echo $DATASTORE_READONLY_PASSWORD` '\''
 
 CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD :datastore_ro_password;
-CREATE DATABASE datastore OWNER ckan_local ENCODING 'utf-8';
+CREATE DATABASE datastore_default OWNER ckan_default ENCODING 'utf-8';
--- postgres/docker-entrypoint-initdb.d/20_setup_test_databases.sql
+++ postgres/docker-entrypoint-initdb.d/20_setup_test_databases.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE ckan_test OWNER ckan_local ENCODING 'utf-8';
-CREATE DATABASE datastore_test OWNER ckan_local ENCODING 'utf-8';
+CREATE DATABASE ckan_test OWNER ckan_default ENCODING 'utf-8';
+CREATE DATABASE datastore_test OWNER ckan_default ENCODING 'utf-8';
