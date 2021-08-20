c2c86eb6c338a36c0a2970d67ee3eaf9224b38b4
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-08-20 17:51:19 +0200

    Update datastore user

--- postgres/docker-entrypoint-initdb.d/10_create_datastore.sql
+++ postgres/docker-entrypoint-initdb.d/10_create_datastore.sql
@@ -1,4 +1,4 @@
 \set datastore_ro_password '\'' `echo $DATASTORE_READONLY_PASSWORD` '\''
 
-CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD :datastore_ro_password;
+CREATE ROLE datastore_default NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD :datastore_ro_password;
 CREATE DATABASE datastore_default OWNER ckan_default ENCODING 'utf-8';
